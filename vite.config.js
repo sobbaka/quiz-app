d41a0e55b16f9194d587edcddc3766967e0d5eb5
sobbaka <a.bagaev1989@gmail.com>
2023-12-31 10:17:58 +0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true
+    },
+    open: true
+  },
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+  plugins: [react()],
+})
