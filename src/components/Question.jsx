65ecd1f8bd86f9a4f0c9989305cb91c836b0d1d1
sobbaka <a.bagaev1989@gmail.com>
2024-01-02 13:41:45 +0400

    Change answers rendering

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,23 @@
-export default function Question({ question }) {
+import { useEffect } from "react";
+import Options from "./Options"
+
+const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+};
+
+
+
+export default function Question({ questions, index, dispatch, userAnswer, answers }) {
+    const question = questions[index]
+    // const answers = shuffle([question.correct_answer, ...question.incorrect_answers])
+
+    useEffect(() => dispatch({ type: 'setAnswers', payload: shuffle([question.correct_answer, ...question.incorrect_answers]) }), [question])
+
+
     return (
-        <h2>{question}</h2>
+        <>
+            <h2>{question.question}</h2>
+            <Options question={question} dispatch={dispatch} userAnswer={userAnswer} answers={answers} />
+        </>
     )
 }
