a47cfd42a23061eb54e09708340f38fb4a69aa6d
sobbaka <a.bagaev1989@gmail.com>
2024-01-02 13:42:33 +0400

    Add active, dataFailed, setAnswers, nextQuestion actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useReducer } from "react"
 import StartScreen from "./components/StartScreen"
+import Question from "./components/Question"
 
 const initialState = {
   questions: [],
   // loading, error, ready, active, finished
   status: 'loading',
   index: 0,
-  answer: null,
+  userAnswer: null,
+  answers: [],
   points: 0,
   highscore: 0,
   time: 30
@@ -19,8 +21,22 @@ function reducer(state, action) {
       return { ...state }
     case 'dataReceived':
       return { ...state, status: 'ready', questions: action.payload, time: 30 * action.payload.length }
+    case 'dataFailed':
+      return { ...state, status: 'loadFailed' }
     case 'active':
       return { ...state, status: 'active' }
+    case 'setAnswers':
+      return { ...state, answers: action.payload, userAnswer: null }
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1 }
+    case 'answer':
+      return {
+        ...state,
+        userAnswer: action.payload,
+        points: action.payload === state.questions[state.index].correct_answer ?
+          state.points + 1 :
+          state.points
+      }
     case 'error':
       return { ...state }
     default:
@@ -32,7 +48,7 @@ function reducer(state, action) {
 
 function App() {
 
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index, answers, userAnswer }, dispatch] = useReducer(reducer, initialState)
 
   useEffect(function () {
     fetch('https://opentdb.com/api.php?amount=15&category=11&difficulty=easy&type=multiple')
@@ -45,7 +61,8 @@ function App() {
   return (
     <div className="container d-flex flex-column align-items-center mx-auto">
       {status === 'ready' && <StartScreen dispatch={dispatch} />}
-      {status === 'active' && <h1>Active</h1>}
+      {status === 'active' && <Question dispatch={dispatch} questions={questions} index={index} userAnswer={userAnswer} answers={answers} />}
+      {status === 'loadFailed' && <h2>Data load failed</h2>}
     </div>
   )
 }
