99f90c28278680837bd53e6bee3203f8bd016464
sobbaka <a.bagaev1989@gmail.com>
2024-01-03 15:32:09 +0400

    add space between answers

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 export default function Button({ question, answer, dispatch, userAnswer, hasAnswered }) {
 
-
     return (
         <button
-            className={`btn btn-primary ${hasAnswered && (question.correct_answer === answer ? " btn-success" : userAnswer === answer ? " btn-danger" : " btn-warning")}`}
+            className={`btn btn-primary mx-2
+            ${hasAnswered &&
+                (question.correct_answer === answer ? " btn-success" :
+                    userAnswer === answer ? " btn-danger" : " btn-warning")}`}
             onClick={() => dispatch({ type: 'answer', payload: answer })}
         >
             {answer}
