1a6e858474b5424472fa103a12adb364b0ecab71
sobbaka <a.bagaev1989@gmail.com>
2024-01-03 15:33:42 +0400

    add spacing to next button

--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,3 +1,3 @@
 export default function NextButton({ dispatch }) {
-    return <button className="btn btn-info" onClick={() => dispatch({ type: 'nextQuestion' })}>Next question</button>
+    return <button className="btn btn-info mt-3 mb-2" onClick={() => dispatch({ type: 'nextQuestion' })}>Next question</button>
 }
