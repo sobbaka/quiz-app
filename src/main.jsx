82e9daffc55a33676adf3f5507df532b7358fa29
sobbaka <a.bagaev1989@gmail.com>
2024-01-04 16:32:41 +0400

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import * as bootstrap from 'bootstrap'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
