7dae5f8e89e1ee554d5e7d645d9108052325478d
sobbaka <a.bagaev1989@gmail.com>
2024-01-04 16:32:28 +0400

    add spacing

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,7 +1,7 @@
 export default function StartScreen({ dispatch }) {
     return (
         <>
-            <h1 className="mt-4 mb-3">Welcome to our Quiz App. Are you ready?</h1>
+            <h1 className="mb-3 text-center custom__h1-start">Welcome to our Quiz App. Are you ready?</h1>
             <button className="btn btn-outline-primary btn-lg" onClick={() => dispatch({ type: 'active' })}>Start quiz</button>
         </>
 
